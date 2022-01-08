1348a30520f75a2c587ff6da4998af6829d200db
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-01-08 11:41:37 +0900

    Use a pipe to generate a video

--- lib/test_recorder/cdp_recorder.rb
+++ lib/test_recorder/cdp_recorder.rb
@@ -1,10 +1,11 @@
+require "open3"
 require "fileutils"
+require "tempfile"
 
 module TestRecorder
   class CdpRecorder
     def initialize(enabled:)
       @enabled = enabled
-      @tmpdir = nil
       setup
     end
 
@@ -17,19 +18,20 @@ module TestRecorder
       enabled = @enabled if enabled.nil?
       return unless enabled
 
-      @tmpdir = Dir.mktmpdir("testrecorder")
-      @counter = 1
+      @tmp_video = Tempfile.open(["testrecorder", ".mp4"])
+      cmd = "ffmpeg -loglevel error -f image2pipe -avioflags direct -fpsprobesize 0 -probesize 32 -analyzeduration 0 -c:v mjpeg -i - -y -an -r 25 -qmin 0 -qmax 50 -crf 8 -deadline realtime -speed 8 -b:v 1M -threads 1 #{@tmp_video.path}"
+      @stdin, @wait_thrs = *Open3.pipeline_w(cmd)
+      @stdin.set_encoding("ASCII-8BIT")
 
       @page = page
       @page.driver.browser.devtools.page.enable
 
       @page.driver.browser.devtools.page.on(:screencast_frame) do |event|
-        decoded_data = Base64.decode64(event["data"])
-        filename = "%010d.jpeg" %  @counter
-        if Dir.exist?(@tmpdir)
-          IO.binwrite("#{File.join(@tmpdir, filename)}", decoded_data)
-          @counter += 1
+        unless @stdin.closed?
+          decoded_data = Base64.decode64(event["data"])
+          @stdin.print(decoded_data)
         end
+
         @page.driver.browser.devtools.page.screencast_frame_ack(session_id: event["sessionId"])
       end
 
@@ -37,25 +39,19 @@ module TestRecorder
     end
 
     def stop_and_discard
-      FileUtils.rm_rf(@tmpdir) unless @tmpdir.nil?
+      @stdin.close
     end
 
     def stop_and_save(filename)
-      return if @tmpdir.nil? || @page.nil?
+      return if @page.nil?
 
-      @page.driver.browser.devtools.page.stop_screencast
+      @stdin.close
+      @wait_thrs.each(&:join)
+
       video_path = File.join(@video_dir, filename)
-
-      args = %W(-loglevel error -f image2 -avioflags direct -fpsprobesize 0
-        -probesize 32 -analyzeduration 0 -c:v mjpeg -i #{File.join(@tmpdir, "%010d.jpeg")}
-        -y -an -r 25 -qmin 0 -qmax 50 -crf 8 -deadline realtime -speed 8 -b:v 1M
-        -threads 1 #{video_path})
-      system("ffmpeg", *args, exception: true)
+      FileUtils.copy(@tmp_video.path, video_path)
+      @tmp_video.close(true)
       video_path
-    rescue => e
-      $stderr.puts("[TestRecorder] ffmpeg failed: #{e.message}")
-    ensure
-      FileUtils.rm_rf(@tmpdir)
     end
   end
 end
