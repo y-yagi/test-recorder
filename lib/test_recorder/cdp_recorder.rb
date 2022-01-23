938cff9a12723c524cc8bfb2e33a60f42a51b351
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-01-23 12:57:04 +0900

    Create files in the other process

--- lib/test_recorder/cdp_recorder.rb
+++ lib/test_recorder/cdp_recorder.rb
@@ -5,6 +5,7 @@ module TestRecorder
     def initialize(enabled:)
       @enabled = enabled
       @tmpdir = nil
+      @worker_pid = nil
       setup
     end
 
@@ -18,26 +19,35 @@ module TestRecorder
       return unless enabled
 
       @tmpdir = Dir.mktmpdir("testrecorder")
-      @counter = 1
-
       @page = page
-      @page.driver.browser.devtools.page.enable
 
-      @page.driver.browser.devtools.page.on(:screencast_frame) do |event|
-        decoded_data = Base64.decode64(event["data"])
-        filename = "%010d.jpeg" %  @counter
-        if Dir.exist?(@tmpdir)
-          IO.binwrite("#{File.join(@tmpdir, filename)}", decoded_data)
-          @counter += 1
+      @worker_pid = fork do
+        @page.driver.browser.devtools.page.enable
+        @counter = 1
+
+        @page.driver.browser.devtools.page.on(:screencast_frame) do |event|
+          decoded_data = Base64.decode64(event["data"])
+          filename = "%010d.jpeg" %  @counter
+          if Dir.exist?(@tmpdir)
+            IO.binwrite("#{File.join(@tmpdir, filename)}", decoded_data)
+            @counter += 1
+          end
+          @page.driver.browser.devtools.page.screencast_frame_ack(session_id: event["sessionId"])
         end
-        @page.driver.browser.devtools.page.screencast_frame_ack(session_id: event["sessionId"])
-      end
 
-      @page.driver.browser.devtools.page.start_screencast(format: "jpeg", quality: 90)
+        @page.driver.browser.devtools.page.start_screencast(format: "jpeg", quality: 90)
+
+        Signal.trap(:TERM) do
+          @page.driver.browser.devtools.page.stop_screencast
+        end
+
+        sleep
+      end
     end
 
     def stop_and_discard
       FileUtils.rm_rf(@tmpdir) unless @tmpdir.nil?
+      Process.kill(:SIGTERM, @worker_pid) if @worker_pid
     end
 
     def stop_and_save(filename)
@@ -56,6 +66,7 @@ module TestRecorder
       $stderr.puts("[TestRecorder] ffmpeg failed: #{e.message}")
     ensure
       FileUtils.rm_rf(@tmpdir)
+      Process.kill(:SIGTERM, @worker_pid) if @worker_pid
     end
   end
 end
