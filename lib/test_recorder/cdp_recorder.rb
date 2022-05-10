f0341547f6b61ad90c4c31f242c583a92877220d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-05-11 08:50:24 +0900

    Revert "Merge pull request #14 from y-yagi/use-pipe"
    This reverts commit acb09d46c4ecfcb17e378bd19bf7887be537a40e, reversing
    changes made to 285f5adb633b8156b1bf61a88aa3509f3f5ff3f0.
    
    Reason: Some pictures can't record correctly with this approach.

--- lib/test_recorder/cdp_recorder.rb
+++ lib/test_recorder/cdp_recorder.rb
@@ -1,11 +1,10 @@
-require "open3"
 require "fileutils"
-require "tempfile"
 
 module TestRecorder
   class CdpRecorder
     def initialize(enabled:)
       @enabled = enabled
+      @tmpdir = nil
       setup
     end
 
@@ -18,20 +17,19 @@ module TestRecorder
       enabled = @enabled if enabled.nil?
       return unless enabled
 
-      @tmp_video = Tempfile.open(["testrecorder", ".mp4"])
-      cmd = "ffmpeg -loglevel quiet -f image2pipe -avioflags direct -fpsprobesize 0 -probesize 32 -analyzeduration 0 -c:v mjpeg -i - -y -an -r 25 -qmin 0 -qmax 50 -crf 8 -deadline realtime -speed 8 -b:v 1M -threads 1 #{@tmp_video.path}"
-      @stdin, @wait_thrs = *Open3.pipeline_w(cmd)
-      @stdin.set_encoding("ASCII-8BIT")
+      @tmpdir = Dir.mktmpdir("testrecorder")
+      @counter = 1
 
       @page = page
       @page.driver.browser.devtools.page.enable
 
       @page.driver.browser.devtools.page.on(:screencast_frame) do |event|
-        unless @stdin.closed?
-          decoded_data = Base64.decode64(event["data"])
-          @stdin.print(decoded_data)
+        decoded_data = Base64.decode64(event["data"])
+        filename = "%010d.jpeg" %  @counter
+        if Dir.exist?(@tmpdir)
+          IO.binwrite("#{File.join(@tmpdir, filename)}", decoded_data)
+          @counter += 1
         end
-
         @page.driver.browser.devtools.page.screencast_frame_ack(session_id: event["sessionId"])
       end
 
@@ -39,19 +37,25 @@ module TestRecorder
     end
 
     def stop_and_discard
-      @stdin.close
+      FileUtils.rm_rf(@tmpdir) unless @tmpdir.nil?
     end
 
     def stop_and_save(filename)
-      return "" if @page.nil?
+      return if @tmpdir.nil? || @page.nil?
 
-      @stdin.close
-      @wait_thrs.each(&:join)
-
+      @page.driver.browser.devtools.page.stop_screencast
       video_path = File.join(@video_dir, filename)
-      FileUtils.copy(@tmp_video.path, video_path)
-      @tmp_video.close(true)
+
+      args = %W(-loglevel error -f image2 -avioflags direct -fpsprobesize 0
+        -probesize 32 -analyzeduration 0 -c:v mjpeg -i #{File.join(@tmpdir, "%010d.jpeg")}
+        -y -an -r 25 -qmin 0 -qmax 50 -crf 8 -deadline realtime -speed 8 -b:v 1M
+        -threads 1 #{video_path})
+      system("ffmpeg", *args, exception: true)
       video_path
+    rescue => e
+      $stderr.puts("[TestRecorder] ffmpeg failed: #{e.message}")
+    ensure
+      FileUtils.rm_rf(@tmpdir)
     end
   end
 end
